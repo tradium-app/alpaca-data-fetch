fe82e59058c73e6326908d8a37bbf6f8857a0948
Suraj Shrestha <syuraj@gmail.com>
2020-11-27 17:51:08 -0500

    spy data downloaded

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { downloadBars } = require('./stock-downloader')
 
 ;(async () => {
-	await downloadBars('NFLX', '2015-01-02T09:30:00-04:00', '2020-07-19T09:30:00-04:00', 1000).catch((err) => {
+	await downloadBars('SPY', '2020-01-01T00:10:00', '2020-11-22T12:10:00', 1).catch((err) => {
 		console.log('printing err', err)
 	})
 	console.log('download completed')
--- stock-downloader.js
+++ stock-downloader.js
@@ -6,7 +6,7 @@ const moment = require('moment-business-days')
 require('dotenv').config()
 const alpaca = new Alpaca({ paper: false })
 
-async function downloadBars(stock = 'AAPL', from_date = '2020-07-16T09:30:00-04:00', to_date = '2020-07-16T09:30:00-04:00', delaySeconds = 5000) {
+async function downloadBars(stock = 'AAPL', from_date = '2020-07-16T09:30:00-04:00', to_date = '2020-07-16T09:30:00-04:00', delaySeconds = 5) {
 	let current_date = moment(from_date)
 	let end_date = ''
 	let promises = []
@@ -52,7 +52,7 @@ function delay(seconds) {
 	return new Promise((resolve) => {
 		setTimeout(() => {
 			resolve()
-		}, seconds)
+		}, seconds * 1000)
 	})
 }
 
